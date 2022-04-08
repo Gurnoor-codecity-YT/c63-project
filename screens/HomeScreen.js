d864e4d646a37ce901ced0dca2981d7cdc84c883
Gurnoor-codecity-YT <80161675+Gurnoor-codecity-YT@users.noreply.github.com>
2022-04-08 15:27:03 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+
+export default class HomeScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = { word: '', definition: '', phonetics: '' };
+  }
+  getWord = (word) => {
+    var url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
+    return fetch(url)
+      .then((data) => {
+        return data.json();
+      })
+      .then((response) => {
+        console.log(response);
+        //var responseObject = JSON.parse(response);
+        var word = response[0].word;
+        console.log(word);
+        var definition = response[0].meanings[0].definitions[0].definition;
+        console.log(definition);
+        this.setState({
+          word: word.trim(),
+          definition: definition.trim(),
+        });
+      });
+  };
+
+  render() {
+    return (
+      <View>
+        <Header
+          backgroundColor={'black'}
+          centerComponent={{
+            text: 'Pocket Dictionary',
+
+            style: { color: '#12a3e6', fontSize: 30, fontFamily: 'Rubik Moonrocks' },
+          }}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={(text) => {
+            this.setState({
+              text: text,
+              isSearchedPressed: false,
+              word: 'loading....',
+              lexicalCategory: '',
+              examples: [],
+              definition: '',
+            });
+          }}
+        />
+
+        <TouchableOpacity
+          style={styles.searchButton}
+          onPress={() => {
+            this.setState({ isSearchedPressed: true });
+            this.getWord(this.state.text);
+          }}>
+          <Text style={styles.textIn}> search </Text>{' '}
+        </TouchableOpacity>
+
+        <Text style={{ fontSize: 20 }}>{this.state.word}</Text>
+        <Text style={{ fontSize: 20 }}>{this.state.definition}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputBox: {
+    marginTop: 50,
+    width: '80%',
+    alignSelf: 'center',
+    height: 45,
+    textAlign: 'center',
+    borderWidth: 4,
+    borderColor: '#12a3e6',
+    outline: 'none',
+    borderRadius:20,
+  backgroundColor:"black",
+  color:"#12a3e6",
+  fontSize:'30'
+  },
+  searchButton: {
+    width: '30%',
+    height: 50,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: '#12a3e6',
+  backgroundColor:'black'
+  },
+  textIn: {
+    textAlign: 'center',
+    fontFamily: 'Rubik Moonrocks',
+    fontSize: 25,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color:'#12a3e6',
+  
+
+  },
+});
